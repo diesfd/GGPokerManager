902fa6974c0c425165dca88194ceb08ef075acd6
zhangyunxuan <yunxuanzhang@live.com>
2021-05-23 01:14:55 +0800

    finished rake counter

---
+++ RakeCounter.py
@@ -0,0 +1,34 @@
+import os
+import re
+
+
+def count():
+    rake = 0
+
+    base_dir = '/Users/zhangyunxuan/Documents/扑克/handData/'
+    for folder_name in os.listdir(base_dir):
+        if not os.path.isdir(base_dir + folder_name):
+            continue
+
+        for file_name in os.listdir(base_dir + folder_name):
+            f = open(base_dir + folder_name + '/' + file_name, "r")
+            content = f.read().split('Poker Hand #')
+
+            for hand in content:
+                result = re.search('Hero \([A-Za-z][A-Za-z\s]*\) collected', hand)
+                if result is None:
+                    result = re.search('Hero \([A-Za-z][A-Za-z\s]*\) won', hand)
+                if result is None:
+                    result = re.search('Hero \([A-Za-z][A-Za-z\s]*\) showed \[[A-Za-z1-9\s]*\] and won', hand)
+
+                if result is None:
+                    continue
+
+                rakeStr = re.findall(r'Rake \$(.*) \| Jackpot', hand)
+                rake += float(rakeStr[0])
+
+    return rake
+
+
+if __name__ == '__main__':
+    print(count())
